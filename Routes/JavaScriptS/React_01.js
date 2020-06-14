a2929bf68e53ed6a405743e035cce32c8ebc692d
InExSu <36605404+InExSu@users.noreply.github.com>
2020-06-14 16:17:55 +0300

    Update React_01.js

--- Routes/JavaScriptS/React_01.js
+++ Routes/JavaScriptS/React_01.js
@@ -29,7 +29,7 @@ ReactDOM.render(e(LikeButton), domContainer);
 function tick() {
   const element = (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>React.JS</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
