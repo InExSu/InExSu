9d49dd03a85a9cec6d944509234fb0cfcfc8b045
InExSu <36605404+InExSu@users.noreply.github.com>
2020-06-14 13:03:08 +0300

    Create React_01.js

---
+++ Routes/JavaScriptS/React_01.js
@@ -0,0 +1,27 @@
+// образец из https://gist.githubusercontent.com/gaearon/0b180827c190fe4fd98b4c7f570ea4a8/raw/b9157ce933c79a4559d2aa9ff3372668cce48de7/LikeButton.js
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+// 
+const domContainer = document.querySelector('#React_01_container'); //поиск div контейнера
+ReactDOM.render(e(LikeButton), domContainer);
+
